424c66e0d1784a03c34929e5b6fd5e922f08ecfb
Asher Dawes <asher.dawes@gmail.com>
2018-05-17 12:22:41 -0700

    Adding debug symbols

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -14,7 +14,6 @@ $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
 echo $(package)_file_name
 $(package)_sha256_hash=48621912c242753ba37cad5145df375eeba41c81079df46f93ffb4896542e8fd
 endif
-
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
 endef
