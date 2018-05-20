f1f19a82cd34e56ea0ab033b908c76bd6d4e1a79
Asher Dawes <asher.dawes@gmail.com>
2018-05-20 14:23:13 -0700

    Testing Agama builds

--- build-win-agama.sh
+++ build-win-agama.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-
+./zcutil/build-win.sh -j2
+rm -rf agama || sudo rm -rf agama || echo "No clean needed"
+git clone https://github.com/asherda/agama --recursive --branch dev --single-branch || true
+cd agama
+./binary_artifacts.sh || true
+npm install electron-packager -g || sudo npm install electron-packager -g
+npm install electron -g --unsafe-perm=true || sudo npm install electron -g --unsafe-perm=true
+cd gui
+rm -rf EasyDEX-GUI
+git clone -b dev --single-branch https://github.com/VerusCoin/EasyDEX-GUI.git || true
+cd EasyDEX-GUI
+npm install && npm install webpack webpack-dashboard
+cd react
+npm install
+npm run build
+cd ../../..
+npm install webpack
+ls
+cd ..
 yes | cp -rf /src/{komodod.exe,komodo-cli.exe,komodo-tx.exe} agama/assets/bin/win64
 cd agama
 npm install
