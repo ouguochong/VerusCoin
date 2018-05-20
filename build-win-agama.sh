5a3661c6980cf703c042a9d6407fa0ae2a2d43a7
Asher Dawes <asher.dawes@gmail.com>
2018-05-20 14:15:17 -0700

    Testing Agama builds

---
+++ build-win-agama.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+yes | cp -rf /src/{komodod.exe,komodo-cli.exe,komodo-tx.exe} agama/bin/assets/win64
+cd agama
+npm install
+electron-packager . --platform=win32 --arch=x64 --icon=assets/icons/agama_icons/agama_app_icon.ico --out=build/ --buildVersion=0.3 --ignore=assets/bin/osx --ignore=assets/bin/linux64 --overwrite
+cd build
+zip -r Agama-Win.zip Agama-win32-x64/
+
+
