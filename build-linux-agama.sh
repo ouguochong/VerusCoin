e4866a2b13205e2c56b5c46c35c0fda51ac54c64
Asher Dawes <asher.dawes@gmail.com>
2018-05-20 13:54:03 -0700

    Testing Agama builds

---
+++ build-linux-agama.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+./zcutil/build-win.sh -j2
+git clone https://github.com/asherda/agama --recursive --branch dev --single-branch || true
+cd agama
+./binary_artifacts.sh || true
+npm install electron-packager -g || sudo npm install electron-packager -g
+npm install electron -g --unsafe-perm=true || sudo npm install electron -g --unsafe-perm=true
+cd gui
+rm -rf EasyDEX-GUI
+git clone -b dev --single-branch https://github.com/VerusCoin/EasyDEX-GUI.git || true
+cd EasyDEX-GUI
+npm install && npm install webpack webpack-dashboard
+cd react
+npm install
+npm run build
+cd ../../..
+npm install webpack
+ls
+cd ..
+yes | cp -rf /src/{komodod.exe,komodo-cli.exe,komodo-tx.exe} /agama/bin/assets/win64
+cd agama
+npm install
+electron-packager . --platform=win32 --arch=x64 --icon=assets/icons/agama_icons/agama_app_icon.ico --out=build/ --buildVersion=0.3 --ignore=assets/bin/osx --ignore=assets/bin/linux64 --overwrite
+cd build
+zip -r Agama-Win.zip Agama-win32-x64/
+
+
