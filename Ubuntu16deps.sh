10508084d437b85532876836e33a798c3839ec83
Asher Dawes <asher.dawes@gmail.com>
2018-06-05 10:40:59 -0700

    Attempting to use Xenial as the Travis OS

--- Ubuntu16deps.sh
+++ Ubuntu16deps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends --allow install libc6* autoconf automake bsdmainutils build-essential curl pkg-config libc6-dev m4 g++-multilib libtool ncurses-dev unzip python zlib1g-dev wget libssl-dev libprotobuf-dev protobuf-compiler libqrencode-dev libdb++-dev software-properties-common libcurl4-openssl-dev mingw-w64 wine
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends install libc6* autoconf automake bsdmainutils build-essential curl pkg-config libc6-dev m4 g++-multilib libtool ncurses-dev unzip python zlib1g-dev wget libssl-dev libprotobuf-dev protobuf-compiler libqrencode-dev libdb++-dev software-properties-common libcurl4-openssl-dev mingw-w64 wine
